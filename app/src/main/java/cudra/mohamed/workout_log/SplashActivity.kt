3c4178649a81bab0c71c33d6a22c9de69fa0726d
CudraMohamed <ncudramohamed@gmail.com>
2022-05-21 08:55:45 +0300

    first commit

---
+++ app/src/main/java/cudra/mohamed/workout_log/SplashActivity.kt
@@ -0,0 +1,11 @@
+package cudra.mohamed.workout_log
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

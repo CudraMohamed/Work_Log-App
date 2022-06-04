2ea180445a37af335969af4cf0c363ac28d798df
CudraMohamed <ncudramohamed@gmail.com>
2022-06-04 09:51:41 +0300

    added login activity

--- app/src/main/java/cudra/mohamed/workout_log/LogInActivity.kt
+++ app/src/main/java/cudra/mohamed/workout_log/LogInActivity.kt
@@ -2,10 +2,40 @@ package cudra.mohamed.workout_log
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class LogInActivity : AppCompatActivity() {
+    lateinit var btnLogin:Button
+    lateinit var tilEmail:TextInputLayout
+    lateinit var tilPassword:TextInputLayout
+    lateinit var etEmail: TextInputEditText
+    lateinit var etPassword:TextInputEditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+
+        btnLogin=findViewById(R.id.btnLogin)
+        tilEmail=findViewById(R.id.tilEmail)
+        tilPassword=findViewById(R.id.tilPassword)
+        etEmail=findViewById(R.id.etEmail)
+        etPassword=findViewById(R.id.etPassword)
+
+        btnLogin.setOnClickListener {
+            validateLogIn()
+        }
+    }
+    fun validateLogIn(){
+        var email = etEmail.text.toString()
+        var password = etPassword.text.toString()
+
+        if (email.isBlank()){
+            tilEmail.error = getString(R.string.email_required)
+        }
+        if (password.isBlank()){
+            tilPassword.error = getString(R.string.password_required)
+        }
     }
 }
--- app/src/main/java/cudra/mohamed/workout_log/SplashActivity.kt
+++ app/src/main/java/cudra/mohamed/workout_log/SplashActivity.kt
@@ -1,11 +1,16 @@
 package cudra.mohamed.workout_log
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        startActivity(Intent(this,LogInActivity::class.java))
+        finish()
+//        val intent = Intent(this,LogInActivity::class.java)
+//        startActivity(intent)
+//        finish()
     }
 }
